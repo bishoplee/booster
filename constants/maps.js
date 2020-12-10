9a6949b67e3c5c13d4ccfbc4477697a1965ac593
Oladipo Olalekan Olaleye <olaleye@thebluematrix.com>
2020-12-10 10:06:51 +0100

    First commit

---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+//const GOOGLE_API_KEY = "AIzaSyD_lMXvGFGsiO2qhvX7Vs49r-sdICRtrAQ"
+const GOOGLE_API_KEY = "AIzaSyCnqAKlauPXWsHp0L4s2kTX14Efs44bfv8";
+export default GOOGLE_API_KEY
---
+++ screens/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import Products from "./Products";
+import OrderDelivery from "./OrderDelivery";
+
+export {
+    Home,
+    Products,
+    OrderDelivery
+}
